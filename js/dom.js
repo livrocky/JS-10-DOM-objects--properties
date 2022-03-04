e0869e82344024bfb90013e278694ab2cd68d3fd
rokas <rokasra@gmail.com>
2022-03-04 11:16:32 +0200

    pasikeicia i zalia paspaudus ir atgal juoda

--- js/dom.js
+++ js/dom.js
@@ -3,3 +3,31 @@ console.log("dom js");
 const user = "mike";
 
 console.log("user ===", user);
+
+//h2 subtitle
+
+window.subtitleEl = document.body.children[1];
+
+//nudazom pries tai esancio el spalva
+
+subtitleEl.previousElementSibling.style.color = "tomato";
+
+//padidinti font size po subtitleEl esancio elemento antram vaikiniam el
+
+subtitleEl.nextElementSibling.children[1].style.fontSize = "25px";
+
+//nudazyti zaliai elementa ant kurio paspaudem
+
+document.body.addEventListener("click", (event) => {
+  console.log("paspaudei");
+  console.log(`event===`, event); //visas ivyko/evento objektas
+  //elementas ant kurio buvo paspausta
+  const paspaustasEl = event.target;
+  console.log(`event.target ===`, event.target);
+
+  if (paspaustasEl.style.color === "green") {
+    paspaustasEl.style.color = "black";
+  } else {
+    paspaustasEl.style.color = "green";
+  }
+});
---
+++ js/uzd1.js
@@ -0,0 +1,38 @@
+//susikurti 2 objektus stud1 ir stud2
+//name ir marks savybes
+//marks yra studento pazymiu masyvas is 5 nariu
+//sukurti metoda kuris paskaiciuoja ir grazina pazymiu vidurki
+//myAvg
+
+// const stud1 = {
+//   name: "Mike",
+//   //      0  1  2  3   4
+//   marks: [8, 9, 7, 6, 10],
+//   myAvg: function () {
+//     let total = 0;
+//     for (let i = 0; i < this.marks.length; i++) {
+//       total += this.marks[i];
+//     }
+
+//     // console.log("total ===", total);
+//     const avg = total / this.marks.length;
+//     console.log(`${this.name} students avg is ${avg}`);
+//     return avg;
+//   },
+// };
+// stud1.myAvg();
+// stud2.myAvg();
+
+// const students = [stud1, stud2];
+
+// for (let i = 0; i < students.length; i++) {
+//   const currentStudAvg = students[i].myAvg();
+//   studAverages.push(currentStudAvg);
+// }
+// console.log(`studAverages===`, studAverages);
+
+// const stud2 = {
+//   name: "Jill",
+//   marks: [5, 10, 9, 8, 7],
+//   myAvg: () => {},
+// };
