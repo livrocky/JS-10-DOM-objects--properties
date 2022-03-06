e32e7ba6b9ab1bb318eb2ccbe7ab3dc4131a64fc
rokas <rokasra@gmail.com>
2022-03-06 14:24:45 +0200

    question 1 accordion working

--- js/sortAcc.js
+++ js/sortAcc.js
@@ -1,4 +1,11 @@
-//paspaudus ant mygtuko ASC keiciasi i DESC ir atvirksciai
+// paspaudus ant mygtuko ASC keicias i DESC ir atvirksciai
+
+//2. kai paspaudiam ant Question 1 parodom po juo esanti elementa,
+// dar karta paspaudus paslepiam (classList.toggle())
+
+// 3. padarom kad paspaudus ant bet kurio Question el butu parodomas po juo esantis el
+
+// 4. padarom kad kai paspaudziam ant elemento, jei yra atsidares kazkuris kitas, tai uzdarom atsidariusius ir paliekam atidaryta tik ta kuri paspaudem.
 
 const BtnEl = document.getElementById("sort-btn");
 const orderEl = BtnEl.firstElementChild;
@@ -15,6 +22,16 @@ BtnEl.addEventListener("click", (event) => {
   }
 });
 
-//jei dabartine reiksme desc tai norime pakeisti i asc, kitu atveju paliekam desc.
+//2. kai paspaudiam ant Question 1 parodom po juo esanti elementa,
+// dar karta paspaudus paslepiam (classList.toggle())
 
-//PASIZIUREK I DOM JS IR I PRAEITOS PASKAITOS DOM SU SPALVOM. ANALOGISKAS SPRENDIMAS.
+const titleEl = document.querySelector(".acc__title");
+const textEl = document.querySelector(".acc__text");
+console.log(`titleEl===`, titleEl);
+titleEl.addEventListener("click", () => {
+  textEl.classList.toggle("acc__text");
+});
+
+// 3. padarom kad paspaudus ant bet kurio Question el butu parodomas po juo esantis el
+
+// Sukurti eventa , paspaudus ant Object, display class ir su toggle
